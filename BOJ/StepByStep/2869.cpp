ef288d0800436ff55e57d69edd9ea751a72fde9e
ChanGrea <k39335@naver.com>
2020-01-06 23:20:01 +0900

    :sparkles: BOJ 2869 added

---
+++ BOJ/StepByStep/2869.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, v, day;
+    cin >> a >> b >> v;
+
+    day = (v - b - 1) / (a - b) + 1;
+
+    cout << day;
+
+    return 0;
+}

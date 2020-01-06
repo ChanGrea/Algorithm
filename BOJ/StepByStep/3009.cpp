2d494a64c34221d47d1d66d6bb0314b79844c1da
ChanGrea <k39335@naver.com>
2020-01-07 00:33:43 +0900

    :sparkles: BOJ 3008 added

---
+++ BOJ/StepByStep/3009.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[3][2], x, y;
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 2; j++)
+            cin >> a[i][j];
+
+    x = a[0][0] == a[1][0] ? a[2][0] : (a[0][0] == a[2][0] ? a[1][0] : a[0][0]);
+    y = a[0][1] == a[1][1] ? a[2][1] : (a[0][1] == a[2][1] ? a[1][1] : a[0][1]);
+
+    cout << x << ' ' << y;
+
+    return 0;
+}

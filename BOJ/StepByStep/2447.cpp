7b934f2726b2ba383eeb21838890894d7efdc5c3
ChanGrea <k39335@naver.com>
2020-01-09 08:48:21 +0900

    :sparkles: BOJ 2447 added

---
+++ BOJ/StepByStep/2447.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            int dx = i, dy = j;
+            while (dx)
+            {
+                if (dx % 3 == 1 && dy % 3 == 1)
+                    break;
+                dx /= 3, dy /= 3;
+            }
+            putchar(dx ? ' ' : '*');
+        }
+        puts("");
+    }
+    return 0;
+}

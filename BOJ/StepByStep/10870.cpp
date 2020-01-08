451091af43c3ce808f7c388168c371cbf50acea7
ChanGrea <k39335@naver.com>
2020-01-09 08:03:15 +0900

    :sparkles: BOJ 10870 added

---
+++ BOJ/StepByStep/10870.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int fibo(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibo(n - 2) + fibo(n - 1);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << fibo(n) << endl;
+
+    return 0;
+}

dfac903a36c46a1804d24411994226aa5b9c9744
ChanGrea <k39335@naver.com>
2020-01-24 16:34:53 +0900

    :sparkles: BOJ 9663 added

---
+++ BOJ/StepByStep/backtracking/9663.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+
+#define MAX_SIZE 15
+
+using namespace std;
+
+int board[MAX_SIZE];
+int n;
+int cnt;
+
+void path(int y)
+{
+    int ko;
+    if (y == n)
+    {
+        cnt++;
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        ko = 1;
+        for (int j = 0; j < y; j++)
+        {
+            if (board[j] == i || abs(y - j) == abs(i - board[j]))
+            {
+                ko = 0;
+                break;
+            }
+        }
+        if (ko)
+        {
+            board[y] = i;
+            path(y + 1);
+        }
+    }
+}
+
+int main()
+{
+    cin >> n;
+    cnt = 0;
+    path(0);
+
+    cout << cnt << "\n";
+
+    return 0;
+}

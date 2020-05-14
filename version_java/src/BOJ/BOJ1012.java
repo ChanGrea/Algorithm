067d84488c81c70dc777d3e106503a0c618b1856
ChanGrea <k39335@naver.com>
2020-05-15 00:03:49 +0900

    :sparkles: BOJ 1012 added

--- version_java/src/BOJ/BOJ1012.java
+++ version_java/src/BOJ/BOJ1012.java
@@ -5,11 +5,18 @@ import java.io.*;
 public class BOJ1012 {
     private static int M, N, K;
     private static int[][] map;
-    private int[] dx = { -1, 0, 1, 0 };
-    private int[] dy = { 0, 1, 0, -1 };
+    private static boolean[][] visit;
+    private static int[] dx = { -1, 0, 1, 0 };
+    private static int[] dy = { 0, 1, 0, -1 };
 
     private static void dfs(int j, int i) {
-
+        visit[j][i] = true;
+        for(int a = 0; a < 4; a++) {
+            if( i + dx[a] >= 0 && i + dx[a] < M && j + dy[a] >= 0 && j + dy[a] < N) {
+                if(map[j+dy[a]][i+dx[a]] == 1 && !visit[j+dy[a]][i+dx[a]])
+                    dfs(j + dy[a], i + dx[a]);
+            }
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -24,6 +31,7 @@ public class BOJ1012 {
             K = Integer.parseInt(input[2]);
 
             map = new int[N][M];
+            visit = new boolean[N][M];
             for(int i = 0; i < K; i++) {
                 String[] locations = br.readLine().split(" ");
                 map[Integer.parseInt(locations[1])][Integer.parseInt(locations[0])] = 1;
@@ -31,14 +39,21 @@ public class BOJ1012 {
 
             int count = 0;
             for(int i = 0; i < M; i++) {
-                for(int j = 0; j < M; j++) {
-                    if(map[j][i] == 1) {
+                for(int j = 0; j < N; j++) {
+                    if(map[j][i] == 1 && !visit[j][i]) {
                         count++;
                         dfs(j, i);
                     }
                 }
             }
+
+            bw.write(count + "\n");
         }
+
+        bw.flush();
+
+        bw.close();
+        br.close();
     }
 
 }
